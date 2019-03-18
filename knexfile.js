14e799796eed653a17b1243943e0ac74b1ccce23
josharmantrout91 <josh.armantrout@gmail.com>
2019-03-18 17:56:36 -0400

    Dependencies Added and Troubleshooting Knex migration w/ Postgres server.

--- knexfile.js
+++ knexfile.js
@@ -1,37 +1,36 @@
-const pg = require('pg');
+const pg = require("pg");
 
 pg.defaults.ssl = process.env.PG_SSL
-	? !!JSON.parse(String(process.env.PG_SSL))
-	: true;
+  ? !!JSON.parse(String(process.env.PG_SSL))
+  : true;
 
 module.exports = {
-	development: {
-		client: 'pg',
-		connection: {
-			host: 'localhost',
-			user: 'postgres',
-			database: 'postgres'
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './data/migrations',
-			tableName: 'migrations'
-		},
-		seeds: { directory: './data/seeds' }
-	},
+  development: {
+    client: "pg",
+    connection: {
+      host: "localhost",
+      user: "postgres",
+      database: "postgres"
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: { directory: "./data/seeds" }
+  },
 
-	production: {
-		client: 'pg',
-		connection: process.env.DATABASE_URL,
-		pool: {
-			min: 2,
-			max: 10
-		},
-		useNullAsDefault: true,
-		migrations: {
-			directory: './data/migrations',
-			tableName: 'migrations'
-		},
-		seeds: { directory: './data/seeds' }
-	}
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "migrations"
+    },
+    seeds: { directory: "./data/seeds" }
+  }
 };
